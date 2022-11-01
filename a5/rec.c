1d0e1051cc299e331d5469d5ef35ea4edee8ba0f
ProximityTrigger <proximitytrigger@gmail.com>
2022-11-01 11:55:46 -0400

    rec.c

---
+++ a5/rec.c
@@ -0,0 +1,26 @@
+typedef struct Point {
+    int x;
+    int y;
+} point;
+typedef struct Rectangle {
+    point bottomLeft;
+    int width;
+    int height;
+} rectangle;
+static const rectangle empty = { 0 };
+static int max(int a, int b) {
+    return a > b ? a : b;
+}
+static int min(int a, int b) {
+    return a < b ? a : b;
+}
+rectangle intersection(rectangle rects[], int n) {
+    rectangle res = n > 0 ? rects[0] : empty;
+    for (int i = 1; i < n; ++i) {
+        point bot = { max(res.bottomLeft.x, rects[i].bottomLeft.x), max(res.bottomLeft.y, rects[i].bottomLeft.y) };
+        res = (rectangle) { bot, min(res.bottomLeft.x + res.width, rects[i].bottomLeft.x + rects[i].width) - bot.x,
+            min(res.bottomLeft.y + res.height, rects[i].bottomLeft.y + rects[i].height) - bot.y };
+        if (res.width < 0 || res.height < 0) return empty;
+    }
+    return res;
+}
