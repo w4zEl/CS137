c015076ad0a8e1bc4c42f444a274cde7fff0dba0
ProximityTrigger <proximitytrigger@gmail.com>
2022-11-02 10:02:57 -0400

    rec.c: reduce casework since n >= 2.

--- a5/rec.c
+++ a5/rec.c
@@ -7,7 +7,6 @@ typedef struct Rectangle {
     int width;
     int height;
 } rectangle;
-static const rectangle empty = { 0 };
 static int max(int a, int b) {
     return a > b ? a : b;
 }
@@ -15,12 +14,12 @@ static int min(int a, int b) {
     return a < b ? a : b;
 }
 rectangle intersection(rectangle rects[], int n) {
-    rectangle res = n > 0 ? rects[0] : empty;
+    rectangle res = rects[0];
     for (int i = 1; i < n; ++i) {
         point bot = { max(res.bottomLeft.x, rects[i].bottomLeft.x), max(res.bottomLeft.y, rects[i].bottomLeft.y) };
         res = (rectangle) { bot, min(res.bottomLeft.x + res.width, rects[i].bottomLeft.x + rects[i].width) - bot.x,
             min(res.bottomLeft.y + res.height, rects[i].bottomLeft.y + rects[i].height) - bot.y };
-        if (res.width < 0 || res.height < 0) return empty;
+        if (res.width < 0 || res.height < 0) return (rectangle) { 0 };
     }
     return res;
 }
