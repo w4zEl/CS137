d153cd7ab2226ac5a2955d06268000a99f51d57c
ProximityTrigger <proximitytrigger@gmail.com>
2022-11-04 16:27:17 -0400

    runmain.ps1: improve error handling.

--- runmain.ps1
+++ runmain.ps1
@@ -1,3 +1,7 @@
+if ($args.Count -ne 1) {
+    $Host.UI.WriteErrorLine("Incorrect number of arguments. Usage: " + $MyInvocation.MyCommand.Name + " file")
+    exit 1
+}
 $name = $args[0]
 if ($name.EndsWith('.c')) {
     $name = $name.substring(0, $name.LastIndexOf('.c'))
@@ -6,5 +10,5 @@ $out = New-TemporaryFile
 Rename-Item -Path "$($out.FullName)" -NewName "$($out.FullName + '.exe')"
 $out = $out.FullName + '.exe'
 gcc -Wall -pedantic -std=c11 "$name.c" "${name}_main.c" -o "$out"
-& "$out"
+if ($?) { & "$out" }
 Remove-Item "$out"
